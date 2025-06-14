349677b7c47e6c17c4f9eec52d10efdc9e6cc16b
fadi mriri <fmriri2@gmail.com>
2025-06-14 11:43:03 +0100

    code

---
+++ src/main/java/com/tekup/tp1/entites/User.java
@@ -0,0 +1,27 @@
+package com.tekup.tp1.entites;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 charachters")
+    private String username;
+
+    
+}
